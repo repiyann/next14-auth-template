8d7e2db65c6378214c45be7ef255a0a0c481c748
repiyann <muhrepiyan26@gmail.com>
2025-01-10 06:23:15 +0700

    feat: forgot schema

--- src/schema/auth.ts
+++ src/schema/auth.ts
@@ -1,18 +1,32 @@
 import { z } from 'zod'
 
 export const registerSchema = z
-  .object({
-    fullName: z.string().min(8).max(50),
-    email: z.string().min(8).max(50).email(),
-    password: z.string().min(8).max(16),
-    password_confirmation: z.string().min(8).max(16),
-  })
-  .refine((data) => data.password === data.password_confirmation, {
-    message: 'Passwords must match',
-    path: ['password_confirmation'],
-  })
+	.object({
+		fullName: z.string().min(8).max(50),
+		email: z.string().min(8).max(50).email(),
+		password: z.string().min(8).max(16),
+		password_confirmation: z.string().min(8).max(16),
+	})
+	.refine((data) => data.password === data.password_confirmation, {
+		message: 'Passwords must match',
+		path: ['password_confirmation'],
+	})
 
 export const loginSchema = z.object({
-  email: z.string().min(8).max(50).email(),
-  password: z.string().min(8).max(16),
+	email: z.string().min(8).max(50).email(),
+	password: z.string().min(8).max(16),
 })
+
+export const forgotSchema = z.object({
+	email: z.string().min(8).max(50).email(),
+})
+
+export const resetPasswordSchema = z
+	.object({
+		password: z.string().min(8).max(16),
+		password_confirmation: z.string().min(8).max(16),
+	})
+	.refine((data) => data.password === data.password_confirmation, {
+		message: 'Passwords must match',
+		path: ['password_confirmation'],
+	})
